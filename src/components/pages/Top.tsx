6cea8f743ceb59626490ac388453db59e4cea322
yona3 <kou.y.0905@icloud.com>
2021-08-20 23:41:27 +0900

    add: Top style

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,10 +1,19 @@
 import React, { VFC } from 'react';
+import { POSTS } from '../../const/post';
 import { Layout } from '../shared/Layout';
+import { TweetCard } from '../shared/TweetCard';
 
 export const Top: VFC = () => {
   return (
     <Layout>
-      <h1>Top</h1>
+      <h1 className="text-xl font-bold">予約リスト</h1>
+      <div className="space-y-5 mt-6">
+        {POSTS.map((post) => (
+          <div key={post.id}>
+            <TweetCard post={post} />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
---
+++ src/components/shared/TweetCardButton.tsx
@@ -0,0 +1,20 @@
+import React, { VFC } from 'react';
+
+type Props = {
+  children: React.ReactChild;
+  className: string;
+};
+
+export const TweetCardButton: VFC<Props> = ({ children, className }) => {
+  return (
+    <button
+      className={`
+        border w-full py-2 rounded-md 
+        text-white transition text-sm
+        ${className}
+      `}
+    >
+      {children}
+    </button>
+  );
+};
