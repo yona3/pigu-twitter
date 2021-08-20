673ae6f119f768e8811afdf067a11cb053b6f81c
yona3 <kou.y.0905@icloud.com>
2021-08-20 22:31:59 +0900

    add: Header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,9 +1,44 @@
 import React, { VFC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header: VFC = () => {
   return (
-    <header>
-      <h1>pigu twitter</h1>
+    <header className="bg-blue-500">
+      <div
+        className="
+          max-w-5xl h-16 mx-auto px-6 
+          flex justify-between items-center
+        "
+      >
+        <Link to="/">
+          <h1 className="text-white text-xl sm:text-2xl font-normal">
+            pigu twitter
+          </h1>
+        </Link>
+
+        <nav
+          className="
+            flex justify-between items-center text-white
+            w-32 sm:w-40
+          "
+        >
+          <Link to="/workspace">
+            <div className="text-sm sm:text-base">
+              <p>WorkSpace</p>
+            </div>
+          </Link>
+          <Link to="/setting">
+            <div
+              className="
+              bg-gray-500 h-8 w-8 sm:h-10 sm:w-10 rounded-full 
+              flex justify-center items-center text-sm sm:text-base
+            "
+            >
+              <p>K</p>
+            </div>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
