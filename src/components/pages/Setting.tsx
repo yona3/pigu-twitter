7766f1e27ccecc4a20df3fd271f0c7425132be14
yona3 <kou.y.0905@icloud.com>
2021-08-21 15:29:20 +0900

    add: setting page style

--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -4,7 +4,17 @@ import { Layout } from '../shared/Layout';
 export const Setting: VFC = () => {
   return (
     <Layout>
-      <h1>Setting</h1>
+      <h1 className="text-lg sm:text-xl text-center">Setting</h1>
+      <div className="mt-5">
+        <div>
+          <p>Name: yona</p>
+          <p>Email: yonajs208@gmail.com</p>
+        </div>
+
+        <div className="mt-5">
+          <button className="underline text-gray-500">Logout</button>
+        </div>
+      </div>
     </Layout>
   );
 };
