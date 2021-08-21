5af77b32fa5f27117f8933642ddc81e11a32b827
yona3 <kou.y.0905@icloud.com>
2021-08-21 15:58:04 +0900

    remove: test code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect, VFC } from 'react';
+import React, { VFC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { fetchPost } from '../lib/api';
 import { Setting } from './pages/Setting';
 import { Top } from './pages/Top';
 import { Workspace } from './pages/Workspace';
 
 export const App: VFC = () => {
-  const logPost = async (postId: string) => {
-    const doc = await fetchPost(postId);
-    const data = doc.data();
-    console.log(data);
-  };
-
-  useEffect(() => {
-    logPost('3fK5f0bDg');
-  }, []);
-
   return (
     <Router>
       <Switch>
