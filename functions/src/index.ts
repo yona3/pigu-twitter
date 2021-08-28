6619c2e40bd702e6f41f47c68d5d65a21408c297
yona3 <kou.y.0905@icloud.com>
2021-08-28 11:09:13 +0900

    add: todo list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,11 @@ import { functions } from './lib/firebase';
 import { twitterTestFunction } from './modules/testTweet';
 import { autoReserve } from './modules/autoReserve';
 
+// todo v1
+// [ ] tweet
+// [x] autoReserve
+// [ ] tweetNow
+
 export const test = functions
   .region('asia-northeast1')
   .https.onRequest(twitterTestFunction);
