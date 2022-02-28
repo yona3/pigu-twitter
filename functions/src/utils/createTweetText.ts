1f1e02e45cf27148293814e53841fdfbb13f73d7
yona3 <kou.y.0905@icloud.com>
2022-03-01 01:33:39 +0900

    update: tweet text

--- functions/src/utils/createTweetText.ts
+++ functions/src/utils/createTweetText.ts
@@ -4,5 +4,5 @@ export const createTweetText = (post: Post, user: User) => {
   const { title, id } = post;
   const { displayName, uid } = user;
   const shareLink = `https://pigu-ryu.web.app/${uid}/${id}/share`;
-  return `${title}｜${displayName}\n\n#pigu #琉大\n${shareLink}`;
+  return `${title}｜${displayName}\n\n#pigu #琉大 #春から琉大\n${shareLink}`;
 };
